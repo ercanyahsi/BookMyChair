71b7bc162f9846ad24d25dcd44c5b25eb24216c2
Ercan Yahşi <ercanyahsi@icloud.com>
2026-01-03 09:54:07 +0300

    Initial Commit

---
+++ BookMyChair/BookMyChairApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMyChairApp.swift
+//  BookMyChair
+//
+//  Created by Ercan Yahşi on 3.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct BookMyChairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookMyChair/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookMyChair
+//
+//  Created by Ercan Yahşi on 3.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
